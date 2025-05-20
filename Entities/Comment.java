a1e4b945a1decc178e1c1834b6dbfe7b4d21809b
Lucas <us3.lucas@gmail.com>
2025-05-20 14:25:04 -0300

    Update Comment.java

--- Entities/Comment.java
+++ Entities/Comment.java
@@ -1,22 +1,27 @@
 package entities;
 
-
-public class Comment {  
+// Classe que representa um comentário em um post
+public class Comment {
   
+  // Atributo que armazena o texto do comentário
   private String text;
-
+  
+  // Construtor padrão (vazio)
   public Comment () {
   }
-
+  
+  // Construtor com parâmetro para inicializar o comentário com um texto
   public Comment(String text) {
     this.text = text;
   }
-
+  
+  // Método getter para obter o texto do comentário
   public String getText() {
-	return text;
+    return text;
   }
-
+  
+  // Método setter para alterar o texto do comentário
   public void setText(String text) {
-	this.text = text;
-  }  
+    this.text = text;
+  }
 }
